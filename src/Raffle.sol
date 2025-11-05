695e49df37c487688c2725eb6a771b198a21ceea
Afoma <antoinetteorji@gmail.com>
2025-11-05 12:23:24 +0100

    adding chainlink vrf

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -23,6 +23,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.5.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+
 /**
  * @title A sample Raffle contract
  * @author Afoma Orji
@@ -31,28 +34,50 @@ pragma solidity ^0.8.19;
  */ 
 contract Raffle{
     /* Errors */
-    error Raffle_SendMoreToEnterRaffle();
+    error Raffle__SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
     address[] payable private s_players;
 
     /* Events */
     event RaffleEntered(address indexed player);
     
-    construtor(uint256 i_entranceFee) {
+    construtor(uint256 i_entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH sent");
         // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
         if(msg.value < i_entranceFee){
-            revert Raffle_SendMoreToEnterRaffle();
+            revert Raffle__SendMoreToEnterRaffle();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp > i_interval){
+            revert;
+        }
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
 
     /** Getter Functions */
     function getEntranceFee() external view returns(uint256){
